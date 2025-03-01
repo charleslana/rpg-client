b52d1adcd4ac3a6b996fb3d46e3c9f6583f691bd
Charles <charleslanop@gmail.com>
2025-03-01 19:33:26 -0300

    updated

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,9 +9,17 @@ import { createRouter, createWebHistory } from 'vue-router/auto';
 import { setupLayouts } from 'virtual:generated-layouts';
 import { routes } from 'vue-router/auto-routes';
 
+const routesWithNotFound = [
+  ...routes,
+  {
+    path: '/user/',
+    redirect: '/user/status',
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: setupLayouts(routes),
+  routes: setupLayouts(routesWithNotFound),
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
       return savedPosition;
