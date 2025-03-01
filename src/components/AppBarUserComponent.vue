4f708903a87c6cffd3a57f8adac878e61b83f61b
Charles <charleslanop@gmail.com>
2025-03-01 12:10:44 -0300

    updated

--- src/components/AppBarUserComponent.vue
+++ src/components/AppBarUserComponent.vue
@@ -2,6 +2,8 @@
   <v-app-bar
     app
     flat
+    class="app-bar"
+    height="38"
   >
     <v-container class="d-flex align-center justify-space-between">
       <div>1</div>
@@ -14,4 +16,10 @@
   
 <script setup lang="ts"></script>
   
-<style scoped></style>  
+<style scoped>
+.app-bar {
+  border-bottom: 2px solid #4e5254 !important;
+  background: linear-gradient(180deg, #020a13 0%, #002f5e 100%) !important;
+  box-shadow: 0 4px 9px 0 rgba(0, 0, 0, .75) !important;
+}
+</style>  
--- src/components/BottomBarUserComponent.vue
+++ src/components/BottomBarUserComponent.vue
@@ -3,6 +3,8 @@
     app
     flat
     location="bottom"
+    class="app-bar"
+    height="47"
   >
     <v-container class="d-flex align-center justify-space-between">
       <div>1</div>
@@ -15,4 +17,10 @@
     
 <script setup lang="ts"></script>
     
-<style scoped></style>  
+<style scoped>
+.app-bar {
+  border-top: 2px solid #4e5254 !important;
+  background: linear-gradient(180deg, #002f5e 0, #0c0809 100%) !important;
+  box-shadow: 0 4px 9px 0 rgba(0, 0, 0, .75) !important;
+}
+</style>  
